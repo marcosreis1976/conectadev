b06ecd9ccf9064547ff1390bfb70412dfacd55ee
marcosreis1976 <marcosaurelio@eris.net.br>
2020-06-14 10:32:12 -0300

    Código Inicial

---
+++ src/pages/Home/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './style.css';
+
+function Home() {
+
+    return (
+        <div>
+            <header className="header">
+                <div className="toolbar">
+                    <div>
+                        <a href="">Conecta Dev</a>
+                        <input type="text"></input>
+                    </div>
+                    <div>
+                        <button>Novo Post</button>
+                        <span>img1</span>
+                        <span>img2</span>
+                    </div>
+                </div>
+            </header>
+            
+            <div className="toolbar"></div>
+            <main className="main">
+                <div className="navbar">
+                    NavBar                    
+                </div>
+                <div className="feed">
+                    Feed
+                </div>
+
+            </main>
+        </div>
+    )
+}
+
+export default Home;
