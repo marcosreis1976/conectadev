85c1d0d99e6b6bb78c185a291ee27f02b30ffa6e
Marcos Reis <marcosaurelio@eris.net.br>
2020-06-14 12:37:26 -0300

    Adicionado AppBar com Material UI

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import Header from './components/Header';
-import './style.css';
+import NavBar from './components/NavBar';
+import Feed from './components/Feed';
+//import './style.css';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    main: {
+        height: '100vh',
+        display: 'flex',
+        width: '1200px',
+        margin: '0 auto',
+    }
+});
 
 function Home() {
+    const classes = useStyles();
 
     return (
-        <div>
-            <Header/>
+        <div className={classes.root} >
+            <Header />
             <div className="toolbar"></div>
-            <main className="main">
-                <div className="navbar">
-                    NavBar                    
-                </div>
-                <div className="feed">
-                    Feed
-                </div>
-
+            <main className={classes.main}>
+                <NavBar />
+                <Feed />
             </main>
         </div>
     )
