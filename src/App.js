88131e6afe3350abdeda8928ef1385bb5ce50671
Marcos Reis <marcosaurelio@eris.net.br>
2020-06-14 23:10:03 -0300

    React Router 6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Routes, Route, Router} from 'react-router-dom';
+
+import SignIn from './pages/Signin';
 import Home from './pages/Home';
+import theme from './theme';
 
 function App() {
   return (
-    <Home/>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/sign-in" element={<SignIn/>} />
+          <Route path="*" element={<h1>Not found 404!</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
