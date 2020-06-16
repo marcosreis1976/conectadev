207337738a22db477a2b76df6f20ba63ff9fc0c8
Marcos Reis <marcosaurelio@eris.net.br>
2020-06-15 23:52:52 -0300

    Login with material ui

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,69 +1,147 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
-import { makestyles, makeStyles} from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Avatar from '@material-ui/core/Avatar';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import { useNavigate } from 'react-router-dom';
+import FormHelperText from '@material-ui/core/FormHelperText';
+
 
 const useStyles = makeStyles ((theme) => ({
     /* Flex Containe */
     root: {
-        display: 'flex',
-        flexDirection: 'row',
         height: '100vh'
     },
-    /* Flex Item */
-    left: {
-        background: 'blue',
-        flexGrow: 0,
-        flexBasis: '58%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    /* Flex Item */
-    right: {
-        background: 'yellow',
-        flexGrow: 0,
-        flexBasis: '42%',
-    },
 
-    form: {
-        display: 'flex',
-        flexDirection: 'column',
-        margin: '64px 32px',
-        alignItems: 'center',
-    }
+    image: {
+        backgroundImage: 'url(/images/background.jpg)',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'none',
+        padding: theme.spacing(2),
+        textAlign: 'center',
+      },
+    
+      avatar: {
+          background: theme.palette.primary.main,
+          marginBottom: theme.spacing(1),
+      },
+
+      button: {
+          marginTop: theme.spacing(1),
+      },
+      form: {
+          margin: theme.spacing(2,4),
+      }
+
 }));
+
+function Copyright() {
+    return (
+      <Typography variant="body2" align="center">
+        {'Copyright © '}
+        <a
+          color="inherit"
+          href="https://www.youtube.com/channel/UCVE9-HO_GzLtDK4IGKVSYXA"
+        >
+          Lucas Nhimi
+        </a>{' '}
+        {new Date().getFullYear()}
+      </Typography>
+    );
+  }
+  
 
 function SignIn () {
     const classes = useStyles();
     return (
-        <div className={classes.root}>
-            <div className={classes.left}>
+        <Grid container className={classes.root}>
+            <Grid
+                item
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                md={7}
+                className={classes.image}
+            >
                 <Typography style={{ color: '#fff', fontSize: 35, lineHeight: '45px' }}>
-                    <strong>Simplificando a forma de conectar desenvolvedores de software!</strong>
+                <strong>
+                    Simplificando a forma de conectar desenvolvedores de software!
+                </strong>
                 </Typography>
                 <Typography
-                    variant="body2"
-                    style={{
-                        color: 'rgb(255,255,255, 0.7)',
-                        marginTop: 30,
-                        fontSize: 15,
-                        lineHeight: '30px',
-                    }}
-                    >
-                    Compartilhe seu conhecimento com toda nossa rede de desenvolvedores de
-                    software.
+                variant="body2"
+                style={{
+                    color: 'rgb(255,255,255, 0.7)',
+                    marginTop: 30,
+                    fontSize: 15,
+                    lineHeight: '30px',
+                }}
+                >
+                Compartilhe seu conhecimento com toda nossa rede de desenvolvedores de
+                software.
                 </Typography>
-            </div>
-
-            <div className={classes.right}>
-                <form className={classes.form}>
-                    <h4>Acesso</h4>
-                    <input type="text"/>
-                    <input type="text"/>
-                </form>
-            </div>
-        </div>
+            </Grid>
+            <Grid item md={5}>
+                <Box display="flex" flexDirection="column" alignItems="center" mt={8}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon/>
+                    </Avatar>
+                    <Typography variant="h5">
+                        Acesso
+                    </Typography>
+                    <form className={classes.form}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="E-mail"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Senha"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}
+                        
+                    >
+                        Entrar
+                    </Button>
+                    <Grid container>
+                        <Grid item>
+                            <Link>Esqueceu sua senha?</Link>
+                        </Grid>
+                        <Grid item>
+                        <Link>Não tem uma conta? Registre-se</Link>
+                        </Grid>
+                    </Grid>
+                    </form>
+                    <Copyright />
+                </Box>
+            </Grid>
+        </Grid>
+        
     )
 }
 
