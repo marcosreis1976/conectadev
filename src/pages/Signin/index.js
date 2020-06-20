dc4e6f355943e6e5845b529ecdb2cc787aaccdc2
Marcos Reis <marcosaurelio@eris.net.br>
2020-06-20 11:32:25 -0300

    VsCode Extensions

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -14,159 +14,161 @@ import { useDispatch } from 'react-redux';
 
 import { signIn } from '../../actions/accountActions';
 
+const useStyles = makeStyles((theme) => ({
+   /* Flex Containe */
+   root: {
+      height: '100vh',
+   },
 
-const useStyles = makeStyles ((theme) => ({
-    /* Flex Containe */
-    root: {
-        height: '100vh'
-    },
+   image: {
+      backgroundImage: 'url(/images/background.jpg)',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'none',
+      padding: theme.spacing(2),
+      textAlign: 'center',
+   },
 
-    image: {
-        backgroundImage: 'url(/images/background.jpg)',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'none',
-        padding: theme.spacing(2),
-        textAlign: 'center',
-      },
-    
-      avatar: {
-          background: theme.palette.primary.main,
-          marginBottom: theme.spacing(1),
-      },
+   avatar: {
+      background: theme.palette.primary.main,
+      marginBottom: theme.spacing(1),
+   },
 
-      button: {
-          marginTop: theme.spacing(1),
-      },
-      form: {
-          margin: theme.spacing(2,4),
-      }
-
+   button: {
+      marginTop: theme.spacing(1),
+   },
+   form: {
+      margin: theme.spacing(2, 4),
+   },
 }));
 
 function Copyright() {
-    return (
+   return (
       <Typography variant="body2" align="center">
-        {'Copyright © '}
-        <a
-          color="inherit"
-          href="https://www.youtube.com/channel/UCVE9-HO_GzLtDK4IGKVSYXA"
-        >
-          Lucas Nhimi
-        </a>{' '}
-        {new Date().getFullYear()}
+         {'Copyright © '}
+         <a
+            color="inherit"
+            href="https://www.youtube.com/channel/UCVE9-HO_GzLtDK4IGKVSYXA"
+         >
+            Lucas Nhimi
+         </a>{' '}
+         {new Date().getFullYear()}
       </Typography>
-    );
-  }
-  
-function SignIn () {
-    const classes = useStyles();
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const dispatch = useDispatch();
-    
-    async function handleSignIn() {
-        try {
-            await dispatch(signIn(email, password));
-            navigate('/');
-        } catch (error) {
-            setErrorMessage(error.response.data.message);
-        }
-    }
+   );
+}
 
-    return (
-        <Grid container className={classes.root}>
-            <Grid
-                item
-                container
-                direction="column"
-                justify="center"
-                alignItems="center"
-                md={7}
-                className={classes.image}
+function SignIn() {
+   const classes = useStyles();
+   const navigate = useNavigate();
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const [errorMessage, setErrorMessage] = useState('');
+   const dispatch = useDispatch();
+
+   async function handleSignIn() {
+      try {
+         await dispatch(signIn(email, password));
+         navigate('/');
+      } catch (error) {
+         setErrorMessage(error.response.data.message);
+      }
+   }
+
+   return (
+      <Grid container className={classes.root}>
+         <Grid
+            item
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            md={7}
+            className={classes.image}
+         >
+            <Typography
+               style={{ color: '#fff', fontSize: 35, lineHeight: '45px' }}
             >
-                <Typography style={{ color: '#fff', fontSize: 35, lineHeight: '45px' }}>
-                <strong>
-                    Simplificando a forma de conectar desenvolvedores de software!
-                </strong>
-                </Typography>
-                <Typography
-                variant="body2"
-                style={{
-                    color: 'rgb(255,255,255, 0.7)',
-                    marginTop: 30,
-                    fontSize: 15,
-                    lineHeight: '30px',
-                }}
-                >
-                Compartilhe seu conhecimento com toda nossa rede de desenvolvedores de
-                software.
-                </Typography>
-            </Grid>
-            <Grid item md={5}>
-                <Box display="flex" flexDirection="column" alignItems="center" mt={8}>
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon/>
-                    </Avatar>
-                    <Typography variant="h5">
-                        Acesso
-                    </Typography>
-                    <form className={classes.form}>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="E-mail"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                        value={email}
-                        onChange={(event) => setEmail(event.target.value)}
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Senha"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                    />
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.button}
-                        onClick={handleSignIn}
-                        >
-                        Entrar
-                        </Button>
-                        {errorMessage && (
-                        <FormHelperText error>{errorMessage}</FormHelperText>
-                        )}
-                    <Grid container>
-                        <Grid item>
-                            <Link>Esqueceu sua senha?</Link>
-                        </Grid>
-                        <Grid item>
+               <strong>
+                  Simplificando a forma de conectar desenvolvedores de software!
+               </strong>
+            </Typography>
+            <Typography
+               variant="body2"
+               style={{
+                  color: 'rgb(255,255,255, 0.7)',
+                  marginTop: 30,
+                  fontSize: 15,
+                  lineHeight: '30px',
+               }}
+            >
+               Compartilhe seu conhecimento com toda nossa rede de
+               desenvolvedores de software.
+            </Typography>
+         </Grid>
+         <Grid item md={5}>
+            <Box
+               display="flex"
+               flexDirection="column"
+               alignItems="center"
+               mt={8}
+            >
+               <Avatar className={classes.avatar}>
+                  <LockOutlinedIcon />
+               </Avatar>
+               <Typography variant="h5">Acesso</Typography>
+               <form className={classes.form}>
+                  <TextField
+                     variant="outlined"
+                     margin="normal"
+                     required
+                     fullWidth
+                     id="email"
+                     label="E-mail"
+                     name="email"
+                     autoComplete="email"
+                     autoFocus
+                     value={email}
+                     onChange={(event) => setEmail(event.target.value)}
+                  />
+                  <TextField
+                     variant="outlined"
+                     margin="normal"
+                     required
+                     fullWidth
+                     name="password"
+                     label="Senha"
+                     type="password"
+                     id="password"
+                     autoComplete="current-password"
+                     value={password}
+                     onChange={(event) => setPassword(event.target.value)}
+                  />
+                  <Button
+                     fullWidth
+                     variant="contained"
+                     color="primary"
+                     className={classes.button}
+                     onClick={handleSignIn}
+                  >
+                     Entrar
+                  </Button>
+                  {errorMessage && (
+                     <FormHelperText error>{errorMessage}</FormHelperText>
+                  )}
+                  <Grid container>
+                     <Grid item>
+                        <Link>Esqueceu sua senha?</Link>
+                     </Grid>
+                     <Grid item>
                         <Link>Não tem uma conta? Registre-se</Link>
-                        </Grid>
-                    </Grid>
-                    </form>
-                    <Copyright />
-                </Box>
-            </Grid>
-        </Grid>
-        
-    )
+                     </Grid>
+                  </Grid>
+               </form>
+               <Copyright />
+            </Box>
+         </Grid>
+      </Grid>
+   );
 }
 
 export default SignIn;
--- src/routes/GuestRoute.js
+++ src/routes/GuestRoute.js
@@ -3,18 +3,10 @@ import { Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import { useSelector } from 'react-redux';
 
-function GuestRoute({element: Component, ...rest}) {
-    const account = useSelector(state => state.account);
-    const IsAuthenticated = Boolean(account.user);
-    return (
-        <Route {...rest} element={(
-            IsAuthenticated
-            ? <Home />
-            : Component
-        )}
-        
-        />
-    )
+function GuestRoute({ element: Component, ...rest }) {
+   const account = useSelector((state) => state.account);
+   const IsAuthenticated = Boolean(account.user);
+   return <Route {...rest} element={IsAuthenticated ? <Home /> : Component} />;
 }
 
 export default GuestRoute;
